2b5d448e2b59f00800bb15d2ca80e27f244dcc4f
indigoDanger <dockeryec@g.cofc.edu>
2022-02-18 22:26:35 -0500

    Completed Hw5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,50 +1,77 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: <Indigo Dockery>
+<hw5>.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: <This program uses Python strings and lists, as well as Python indexing and slicing facilities.>
 
 Certification of Authenticity:
-<include one of the following>
-I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
+I certify that this assignment is my own work, but I discussed it with: Brooke at CSL>
 """
 
 
 def name_reverse():
-    pass
-
+    name = input("enter a name ")
+    x = name.split()
+    first = x[0]
+    last = x[1]
+    print(last, first)
 
 def company_name():
-    pass
-
+    company = input("enter company name with a three part domain: ")
+    word = company.split(".")
+    print(word[1], end=" ")
 
 def initials():
-    pass
+    num_students = int(input("enter the number of students in the class:"))
+    for i in range(num_students):
+        first = input("enter students first name: ")
+        last = input("enter the students last name: ")
+        print("initials: ", first[0] + last[0])
 
-
-def names():
-    pass
-
+# def names():
+#     name_list = eval(input("enter a list of names: "))
+#     initial = 0
+#     for i in range(name_list):
+#         initial = name_list.split(".")
+#     print(initial[0], initial[1], initial[2], initial[3], initial[4], initial[5])
 
 def thirds():
-    pass
-
+    num = eval(input("enter the number of sentences: "))
+    sentence = []
+    for i in range(num):
+        sen = input("enter sentence" + str(i+1) + ":")
+        sentence.append(sen)
+    for i in sentence:
+        output = ""
+        for j in range(0, len(i), 3):
+            output = output + i[j:j+1]
+        print(output)
+        output = ""
 
 def word_average():
-    pass
-
+    sum = 0
+    words = input("enter a sentence:")
+    x = words.split()
+    num = len(x)
+    for i in x:
+        sum = sum + len(i)
+    avg = sum / num
+    print(avg)
 
 def pig_latin():
-    pass
-
+    sentence = input("enter a sentence to convert to pig latin: ")
+    word = sentence.split()
+    new = ""
+    for i in word:
+        new += i[1:]+i[0]+"ay "
+        new = new.lower()
+        new = new.capitalize()
+    print(new)
 
 if __name__ == '__main__':
-    # name_reverse()
-    # company_name()
-    # initials()
-    # names()
-    # thirds()
-    # word_average()
-    # pig_latin()
-    pass
+    name_reverse()
+    initials()
+    #names() - doesn't run
+    thirds()
+    word_average()
+    pig_latin()
